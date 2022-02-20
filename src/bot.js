be657c2308c6dc7e25aa38d8209c0e091d4d4abd
xiaqinglin <xia.qinglin@frontsurf.com>
2022-02-20 22:43:10 +0800

    update discord bot in js

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const {Client} = require("discord.js");
-const client = new Client();
+const {Client,Intents} = require("discord.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 console.log(process.env.DISCORDJS_BOT_TOKEN);
 client.login(process.env.DISCORDJS_BOT_TOKEN);
 
