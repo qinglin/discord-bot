998d5bff26cba3031529aa27e25b2744815c8ace
xiaqinglin <xia.qinglin@frontsurf.com>
2022-02-20 23:06:39 +0800

    update discord bot in js

--- src/bot.js
+++ src/bot.js
@@ -4,14 +4,17 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 console.log(process.env.DISCORDJS_BOT_TOKEN);
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.username}!`)
-    message.channel.send({
-        embed: {
-            title: 'Please login into the website',
-            url: 'https://flashsigner.com/connect?success_url=http%3A%2F%2Flocalhost%3A3000%2FFlashsigner%3Faction%3Dlogin%26extra%3D%2522http%253A%252F%252Flocalhost%253A3000%252F%2522&dapp_name=Rostra&dapp_logo=http%3A%2F%2Flocalhost%3A3000%2Flogo512.png',
-        },
+    console.log(`Logged in as ${client.user.username}!`);
+    client.channels.forEach(channel => {
+        if(channel.type === 'text') channel.send({
+            embed: {
+                title: 'Please login into the website',
+                url: 'https://flashsigner.com/connect?success_url=http%3A%2F%2Flocalhost%3A3000%2FFlashsigner%3Faction%3Dlogin%26extra%3D%2522http%253A%252F%252Flocalhost%253A3000%252F%2522&dapp_name=Rostra&dapp_logo=http%3A%2F%2Flocalhost%3A3000%2Flogo512.png',
+            },
+        }).catch(console.error)
     });
 });
+
 
 client.on("message", msg => {
     if (msg.content === "ping") {
