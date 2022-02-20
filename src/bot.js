8f65f75db7b817e5d5467acd99b6e943d3cb4f6d
xiaqinglin <xia.qinglin@frontsurf.com>
2022-02-20 23:21:57 +0800

    update discord bot in js

--- src/bot.js
+++ src/bot.js
@@ -6,20 +6,20 @@ console.log(process.env.DISCORDJS_BOT_TOKEN);
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.username}!`);
     client.channels.fetch('944957927465312299').then(
-        channel => message.channel.send(channel.name)
-        // channel.send({
-        //     embed: {
-        //         title: 'Please login into the website',
-        //         url: 'https://flashsigner.com/connect?success_url=http%3A%2F%2Flocalhost%3A3000%2FFlashsigner%3Faction%3Dlogin%26extra%3D%2522http%253A%252F%252Flocalhost%253A3000%252F%2522&dapp_name=Rostra&dapp_logo=http%3A%2F%2Flocalhost%3A3000%2Flogo512.png',
-        //     },
+        channel => console.log(channel.name)
+
         ).catch(console.error)
     // });
 });
 
 
 client.on("message", msg => {
-    if (msg.content === "ping") {
-        msg.reply("pong");
+    if (msg.content === "hello") {
+        msg.reply({
+            embed: {
+                title: 'Please login into the website',
+                url: 'https://flashsigner.com/connect?success_url=http%3A%2F%2Flocalhost%3A3000%2FFlashsigner%3Faction%3Dlogin%26extra%3D%2522http%253A%252F%252Flocalhost%253A3000%252F%2522&dapp_name=Rostra&dapp_logo=http%3A%2F%2Flocalhost%3A3000%2Flogo512.png',
+            }});
     }
 });
 
